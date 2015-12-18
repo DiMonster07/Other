d7dd0e2dc3ddfdc1cc7ec8a9937e925c730f9863
DiMonster07 <territory_cech@mail.ru>
2015-12-18 19:57:34 +1000

    Add test for MyDatatime

--- DynamicLang/MyDataTime/mydatatime.py
+++ DynamicLang/MyDataTime/mydatatime.py
@@ -2,12 +2,13 @@ from datetime import datetime
 import re, unittest
 
 MINYEAR = 1
-MAXYEAR = 9999999
+MAXYEAR = 9999
 DAYS_IN_MONTH = [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def myprint_time (foo):
-    def wrapper(self): 
-    	return "Functions: " + foo.__name__ + "\nCall time -> " + str(datetime.now()) + '\n'
+def print_time (foo):
+    def wrapper(*args, **kwargs): 
+    	print "Functions: " + foo.__name__ + "\nCall time -> " + str(datetime.now()) + '\n'
+    	return foo(*args, **kwargs)
     return wrapper
 
 def is_leap (year):
@@ -82,9 +83,8 @@ class mydatatime():
 		self._second = second
 		self._microsecond = microsecond
 
+	@print_time
 	def replace(self, year=None, month=None, day=None, hour=None, minute=None, second=None, microsecond=None):
-		year, mounth, day = check_data_fields(year, month, day)
-		hour, minute, second, microsecond = check_time_fields(hour, minute, second, microsecond)
 		if year is None:
 			year = self._year
 		if month is None:
@@ -99,104 +99,78 @@ class mydatatime():
 			second = self._second
 		if microsecond is None:
 			microsecond = self._microsecond
-		return mydatetime(year, month, day, hour, minute, second, microsecond)
-		
+		return mydatatime(year, month, day, hour, minute, second, microsecond)
+	
+	@print_time	
 	def _cmp (self, other):
 		y, ts, m = self._year, int(self.total_seconds()), (self._microsecond) % 10**6
 		y2, ts2, m2 = other._year, int(other.total_seconds()), (other._microsecond) % 10**6
 		return _cmp((y, ts, m), (y2, ts2, m2))
 
+	@print_time
 	def seconds_in_months(self):
 		return (sum(DAYS_IN_MONTH[1: self._month]) + 1) * 86400 if is_leap(self._year) else int(sum(DAYS_IN_MONTH[1: self._month])*86400)
-		
+
+	@print_time	
 	def total_seconds(self):	
 		return ((self.seconds_in_months() + self._day * 86400 + self._hour * 3600 + self._minute * 60 + self._second) * 10**6 + self._microsecond) / 10**6
 
+	@print_time
 	def isoformat(self):
 		return "%04d-%02d-%02d %02d:%02d:%02d:%06d" % (self._year, self._month, self._day, self._hour, self._minute, self._second, self._microsecond)
 
+	@print_time
 	def year(self):
 		return self._year
 
+	@print_time
 	def month(self):
 		return self._month
 	
+	@print_time
 	def day(self):
 		return self._day
 	
+	@print_time
 	def hour(self):
 		return self._hour
 
+	@print_time
 	def minute(self):	
 		return self._minute
 
+	@print_time
 	def second(self):
 		return self._second
 	
+	@print_time
 	def microsecond(self):
 		return self._microsecond
 
+	@print_time
 	def __str__(self):
 		return self.isoformat()
 
+	@print_time	
 	def __lt__(self, other):
 		return self._cmp(other) < 0
 
+	@print_time
 	def __le__(self, other):
 		return self._cmp(other) <= 0		
 
+	@print_time
 	def __eq__(self, other):
 		return self._cmp(other) == 0
 
+	@print_time
 	def __ne__(self, other):
 		return self._cmp(other) != 0 
 
+	@print_time
 	def __gt__(self, other):
 		return self._cmp(other) > 0
 
+	@print_time
 	def __ge__(self, other):
 		return self._cmp(other) >= 0
-
-class Test_valid_init(unittest.TestCase):
-	def setUp(self):
-		self.a = 0 
-	
-	def test_0(self):
-		self.a = mydatatime(2015, 5, 2, 12, 30, 35, 300)
-		self.assertEqual(2015, self.a.year())
-		self.assertEqual(5, self.a.month())
-		self.assertEqual(2, self.a.day())
-		self.assertEqual(12, self.a.hour())
-		self.assertEqual(30, self.a.minute())
-		self.assertEqual(35, self.a.second())
-		self.assertEqual(300, self.a.microsecond())
-
-	#def test_1(self):
-
-
-class Test_is_leap(unittest.TestCase):
-	def setUp(self):
-		self.a = 0
-
-	def test_is_leap(self):
-		self.assertEqual(True, is_leap(2012))
-
-class Test_day_in_month(unittest.TestCase):
-	def setUp(self):
-		self.a = 0
-
-	def test_days_in_month(self):
-		self.assertEqual(29, days_in_month(2012, 2))
-
-class Test_isofromstr(unittest.TestCase):
-	def setUp(self):
-		self.a = 0
-
-	def test_isofromstr(self):
-		self.a = mydatatime(2015, 5, 2, 12, 30, 35, 300)
-		self.assertEqual("2015-05-02 12:30:35:000300", self.a.isoformat())	
-
-
-
-if __name__ == "__main__":
-	unittest.main()
--- DynamicLang/MyDataTime/test.py
+++ DynamicLang/MyDataTime/test.py
@@ -2,14 +2,11 @@ from datetime import datetime
 from mydatatime import *
 import unittest
 
-#a = mydatatime(2015, 5, 2, 12, 30, 30, 300)
 
-class Test(unittest.TestCase):
+class Test_Init(unittest.TestCase):
 	def setUp(self):
-		self.a = 0 
-	
-	def test_valid_init(self):
 		self.a = mydatatime(2015, 5, 2, 12, 30, 35, 300)
+	def test_1 (self):
 		self.assertEqual(2015, self.a.year())
 		self.assertEqual(5, self.a.month())
 		self.assertEqual(2, self.a.day())
@@ -17,15 +14,127 @@ class Test(unittest.TestCase):
 		self.assertEqual(30, self.a.minute())
 		self.assertEqual(35, self.a.second())
 		self.assertEqual(300, self.a.microsecond())
-	
-	def test_is_leap(self):
+
+class Test_is_leap(unittest.TestCase):		
+	def test_1(self):
 		self.assertEqual(True, is_leap(2012))
-		#self.assertEqual(True, is_leap(2013))
+	def test_2(self):
+		self.assertEqual(True, is_leap(2013)) 
 
-	def test_days_in_month(self):
+
+class Test_days_in_month(unittest.TestCase):
+	def test_1(self):
 		self.assertEqual(29, days_in_month(2012, 2))
-		#self.assertEqual(29, days_in_month(2013, 2))
+	def test_2(self):
+		self.assertEqual(29, days_in_month(2013, 2))
+
+class Test_cmp(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual(1, _cmp(3, 2))
+	def test_2(self):
+		self.assertEqual(-1, _cmp(1, 2))
+	def test_3(self):
+		self.assertEqual(0, _cmp(2, 2))
+
+class Test_check_int_field(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual(4, check_int_field(4))
+	def test_2(self):
+		self.assertEqual(TypeError, check_int_field(4.455))
+
+class Test_check_data_fields(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual((4, 4, 4), check_data_fields(4, 4, 4))
+	def test_2(self):
+		self.assertEqual(ValueError, check_data_fields(99999, 4, 4))
+	def test_3(self):
+		self.assertEqual(ValueError, check_data_fields("99999", 4, 4))
+	def test_4(self):
+		self.assertEqual(ValueError, check_data_fields(99999, 15, 60))
+	def test_5(self):
+		self.assertEqual(ValueError, check_data_fields(34, 30, 4))
+	def test_6(self):
+		self.assertEqual(ValueError, check_data_fields(34, 4, 80))
+
+class Test_check_time_fields(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual((12, 10, 4, 56748), check_time_fields(12, 10, 4, 56748))
+	def test_2(self):
+		self.assertEqual(ValueError, check_time_fields(26, 10, 4, 56748))
+	def test_3(self):
+		self.assertEqual(ValueError, check_time_fields("99999", 462, 3241, 999999999))
+	def test_4(self):
+		self.assertEqual(ValueError, check_time_fields(12, 462, 3241, 999999999))
+	def test_5(self):
+		self.assertEqual(ValueError, check_time_fields(12, 10, 3241, 999999999))
+	def test_6(self):
+		self.assertEqual(ValueError, check_time_fields(12, 10, 5, 999999999))
+
+class Test_replace(unittest.TestCase):
+	def setUp(self):
+		self.a = mydatatime(2015, 5, 2, 12, 30, 35, 300)
+		self.b = mydatatime(2015, 5, 2, 12, 30, 35, 300)
+	def test_1(self):
+		self.assertEqual(self.a, self.b.replace())
+	def test_2(self):
+		self.assertEqual(self.a.replace(day = 5), self.b.replace(day = 5))
+
+class Test_cmp_classmethod(unittest.TestCase):
+	def setUp(self):
+		self.a = mydatatime(2015, 5, 2, 12, 30, 35, 300)
+		self.b = mydatatime(2015, 5, 2, 12, 30, 36, 600)
+	def test_1(self):
+		self.assertEqual(-1, self.a._cmp(self.b))
+	def test_2(self):
+		self.assertEqual(0, self.a._cmp(self.b.replace(2015, 5, 2, 12, 30, 35, 300)))
+	def test_3(self):
+		self.assertEqual(0, self.a._cmp(self.b.replace(2013, 4, 2, 12, 30, 35, 300)))
+
+class Test_second_in_month(unittest.TestCase):
+	def setUp(self):
+		self.a = mydatatime(2013, 4, 2, 12, 30, 35, 300)
+	def test_1(self):
+		self.assertEqual(int(sum(DAYS_IN_MONTH[1: 4]))*86400, self.a.seconds_in_months())
+	def test_2(self):
+		self.assertEqual(int(sum(DAYS_IN_MONTH[1: 2]))*86400, (self.a.replace(2013, 2, 2, 12, 30, 35, 300)).seconds_in_months())
+	def test_3(self):
+		self.assertEqual((int(sum(DAYS_IN_MONTH[1: 2])) + 1)*86400, self.a.replace(2012, 2, 2, 12, 30, 35, 300).seconds_in_months())
+
+class Test_isorofmat_str(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual("2012-02-02 12:30:35:000300", mydatatime(2012, 2, 2, 12, 30, 35, 300).isoformat())
+	def test_2(self):
+		self.assertEqual("2012-02-02 12:30:35:000300", str(mydatatime(2012, 2, 2, 12, 30, 35, 300)))
+
+class Test_compairson(unittest.TestCase):
+	def setUp(self):
+		self.a = mydatatime(2012, 2, 2, 12, 30, 35, 300)
+		self.b = mydatatime(2015, 12, 19, 11, 30, 35, 333)
+		self.c = mydatatime(2015, 12, 19, 11, 30, 35, 333)
+	def test_1(self):
+		self.assertEqual(True, self.a < self.b)
+	def test_2(self):
+		self.assertEqual(True, self.a <= self.b)
+	def test_3(self):
+		self.assertEqual(True, self.b == self.c)
+	def test_4(self):
+		self.assertEqual(True, self.a != self.b)
+	def test_5(self):
+		self.assertEqual(True, self.b > self.a)
+	def test_6(self):
+		self.assertEqual(True, self.b >= self.c)
+
+class test_isofromstr(unittest.TestCase):
+	def setUp(self):
+		self.a = mydatatime(2012, 2, 2, 12, 30, 35, 300)
+	def test_1(self):
+		self.assertEqual(self.a, isofromstr("2012-02-02 12:30:35:000300"))
+	def test_2(self):
+		self.assertEqual(self.a, isofromstr("2012-02-02 12:30:35:000333"))
+	def test_3(self):
+		self.assertEqual(self.a, isofromstr("2012-20-02 12:30:35:000300"))
+	
 
 
-if __name__ == "__main__":
-	unittest.main()
+if __name__ == '__main__':
+    unittest.main()
